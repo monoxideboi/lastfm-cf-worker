225ff57fd12274a4aec731bc3e275b2a62cf531d
monoxideboi <38110094+monoxideboi@users.noreply.github.com.>
2024-11-11 15:39:06 -0600

    add rate limit

--- src/index.js
+++ src/index.js
@@ -9,44 +9,55 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
-		const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${env.USER}&api_key=${env.KEY}&limit=1&format=json`;
-		const cacheUrl = new URL(url);
-
-		// Construct the cache key from the cache URL
-		const cacheKey = new Request(cacheUrl.toString());
-		const cache = caches.default;
-
-		// Check whether the value is already available in the cache
-		// if not, you will need to fetch it from origin, and store it in the cache
-		let response = await cache.match(cacheKey);
-
-		if (!response) {
-			console.log(
-				`Response for request url: ${url} not present in cache. Fetching and caching request.`,
-			);
-			// If not in cache, get it from origin
-			let json = await fetch(url).then(data => {
-				return data.json();
-			}).then(json => {
-				return json.recenttracks.track[0];
-			});
-
-			// Must use Response constructor
-			response = Response.json(json);
-
-			// Cache API respects Cache-Control headers. Setting s-max-age to 2
-			// will limit the response to be in cache for 2 seconds max
-			response.headers.append("Cache-Control", "s-maxage=2");
-			
-			response.headers.append("Access-Control-Allow-Origin", "*");
-			response.headers.append("Access-Control-Allow-Headers", "*")
-
-			// Any changes made to the response here will be reflected in the cached value
-			ctx.waitUntil(cache.put(cacheKey, response.clone()));
+	async fetch(req, env, ctx) {
+		const connectingUser = req.headers.get('cf-connecting-ip') || '';
+		console.log(connectingUser);
+		const success = await env.RATE_LIMITER.limit({ key: connectingUser });
+		console.log(success);
+		console.log(success.success);
+		if (!success.success) {
+			console.log(`${connectingUser} exceeded rate limit`);
+			return new Response(`429 Failure – rate limit exceeded for ${connectingUser}`, { status: 429 });
 		} else {
-			console.log(`Cache hit for: ${url}.`);
+			const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${env.USER}&api_key=${env.KEY}&limit=1&format=json`;
+			const cacheUrl = new URL(url);
+
+			// Construct the cache key from the cache URL
+			const cacheKey = new Request(cacheUrl.toString());
+			const cache = caches.default;
+
+			// Check whether the value is already available in the cache
+			// if not, you will need to fetch it from origin, and store it in the cache
+			let response = await cache.match(cacheKey);
+
+			if (!response) {
+				console.log(`Response for request url: ${url} not present in cache. Fetching and caching request.`);
+				// If not in cache, get it from origin
+				let json = await fetch(url)
+					.then((data) => {
+						return data.json();
+					})
+					.then((json) => {
+						console.log(json);
+						return json.recenttracks.track[0];
+					});
+
+				// Must use Response constructor
+				response = Response.json(json);
+
+				// Cache API respects Cache-Control headers. Setting s-max-age to 2
+				// will limit the response to be in cache for 2 seconds max
+				response.headers.append('Cache-Control', 's-maxage=5');
+
+				response.headers.append('Access-Control-Allow-Origin', '*');
+				response.headers.append('Access-Control-Allow-Headers', '*');
+
+				// Any changes made to the response here will be reflected in the cached value
+				ctx.waitUntil(cache.put(cacheKey, response.clone()));
+			} else {
+				console.log(`Cache hit for: ${url}.`);
+			}
+			return response;
 		}
-		return response;
 	},
 };
